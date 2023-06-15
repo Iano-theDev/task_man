a4f7234889a059708bd9878ee5f3006bd6f9a65a
iano-theDev <sinceianmike@gmail.com>
2023-06-15 18:08:19 +0300

    update

--- client/task_manager/src/app/users/users.component.ts
+++ client/task_manager/src/app/users/users.component.ts
@@ -16,36 +16,42 @@ export class UsersComponent {
 
   users: User[] = [
   {
+    id: 1,
     name: "Ian",
     email: "ian@email.com",
     rating: 5,
     avatar: "https://www.universityoffashion.com/blog/wp-content/uploads/2021/10/Picture1-1.png"
   },
   {
+    id: 2,
     name: "John",
     email: "john@example.com",
     rating: 4,
     avatar: "https://www.pngitem.com/pimgs/m/517-5171782_freetoedit-simpsons-stickers-aesthetic-sad-cartoon-illustration-hd.png"
   },
   {
+    id: 3,
     name: "Alice",
     email: "alice@example.com",
     rating: 3,
     avatar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSg8Afnlazwt4y83v-7THjQAqKtC7SVHPrlWW8eB-bSffDupiIg2jiMNHs4P9t_4EkbbIg&usqp=CAU"
   },
   {
+    id: 4,
     name: "Sarah",
     email: "sarah@example.com",
     rating: 4,
     avatar: "https://e0.pxfuel.com/wallpapers/3/812/desktop-wallpaper-sad-cartoon-drawings-simpsons-depressed-simpsons.jpg"
   },
   {
+    id: 5,
     name: "Michael",
     email: "michael@example.com",
     rating: 2,
     avatar: "https://i.pinimg.com/originals/a8/9c/3b/a89c3b9e447e29c17a0707a6ef5049ab.png"
   },
   {
+    id: 6,
     name: "Emily",
     email: "emily@example.com",
     rating: 5,
