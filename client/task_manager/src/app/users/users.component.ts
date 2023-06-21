f9e47a9310359771edef73e6e58e74f138092a43
iano-theDev <sinceianmike@gmail.com>
2023-06-21 09:46:42 +0300

    Separate user component with users

--- client/task_manager/src/app/users/users.component.ts
+++ client/task_manager/src/app/users/users.component.ts
@@ -4,6 +4,7 @@ import { User } from '../models/user.model';
 import { FormsModule } from '@angular/forms';
 import { ChangeColorPipe } from '../shared/changeColor.pipe';
 import { StarComponent } from "../shared/stars/star.component";
+import { UserService } from '../shared/services/users.service';
 
 @Component({
     selector: 'app-users',
@@ -23,6 +24,8 @@ export class UsersComponent implements OnInit {
 
   filteredUsers?: User[]
 
+  constructor(private getUsersService: UserService) {}
+
   private _filterBy: string = '';
 
   get filterUsers(): string {
@@ -35,57 +38,7 @@ export class UsersComponent implements OnInit {
     // console.log(`_filtered: ${this._filterBy}`)
     // console.log(`filtered: ${this.filterUsers}`)
   }
-  users: User[] = [
-    {
-      id: 0,
-      name: "Ian",
-      email: "ian@email.com",
-      rating: 5,
-      avatar: "https://www.universityoffashion.com/blog/wp-content/uploads/2021/10/Picture1-1.png"
-    },
-    {
-      id: 1,
-      name: "Ian",
-      email: "ian@email.com",
-      rating: 5,
-      avatar: "https://www.universityoffashion.com/blog/wp-content/uploads/2021/10/Picture1-1.png"
-    },
-    {
-      id: 2,
-      name: "John",
-      email: "john@example.com",
-      rating: 4,
-      avatar: "https://www.pngitem.com/pimgs/m/517-5171782_freetoedit-simpsons-stickers-aesthetic-sad-cartoon-illustration-hd.png"
-    },
-    {
-      id: 3,
-      name: "Alice",
-      email: "alice@example.com",
-      rating: 3,
-      avatar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSg8Afnlazwt4y83v-7THjQAqKtC7SVHPrlWW8eB-bSffDupiIg2jiMNHs4P9t_4EkbbIg&usqp=CAU"
-    },
-    {
-      id: 4,
-      name: "Sarah",
-      email: "sarah@example.com",
-      rating: 4,
-      avatar: "https://e0.pxfuel.com/wallpapers/3/812/desktop-wallpaper-sad-cartoon-drawings-simpsons-depressed-simpsons.jpg"
-    },
-    {
-      id: 5,
-      name: "Michael",
-      email: "michael@example.com",
-      rating: 2,
-      avatar: "https://i.pinimg.com/originals/a8/9c/3b/a89c3b9e447e29c17a0707a6ef5049ab.png"
-    },
-    {
-      id: 6,
-      name: "Emily",
-      email: "emily@example.com",
-      rating: 5,
-      avatar: "https://i.pinimg.com/564x/d1/49/da/d149da1dac7b0d85b182df84d18b6740.jpg"
-    }
-  ];
+  users: User[] = []
 
   performFilter(searchString: string): User[] {
     searchString = searchString.toLocaleLowerCase()
@@ -103,10 +56,12 @@ export class UsersComponent implements OnInit {
 
   clearSearch(): void {
     this.filterUsers = ''
+    this.getUsersService.getUsers()
   }
 
   ngOnInit(): void {
     this.filterUsers = ''
+    this.users = this.getUsersService.getUsers()
   }
 
   onRatingClick(message: string) :void {
